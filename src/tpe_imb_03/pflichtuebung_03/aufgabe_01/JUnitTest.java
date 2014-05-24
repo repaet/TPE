c16684c3b2996c91d8d98b37561cad981632e057
repaet <rene305@web.de>
2014-05-24 03:19:34 +0200

    removed warnings updated test

--- src/tpe_imb_03/pflichtuebung_03/aufgabe_01/JUnitTest.java
+++ src/tpe_imb_03/pflichtuebung_03/aufgabe_01/JUnitTest.java
@@ -1,12 +1,12 @@
 package tpe_imb_03.pflichtuebung_03.aufgabe_01;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import tpe_imb_03.pflichtuebung_03.aufgabe_01.list.NodeList;
-import tpe_imb_03.pflichtuebung_03.aufgabe_01.list.NodeListImpl;
 import tpe_imb_03.pflichtuebung_03.aufgabe_01.search.Breitensuche;
+import tpe_imb_03.pflichtuebung_03.aufgabe_01.search.SearchStrategy;
 import tpe_imb_03.pflichtuebung_03.aufgabe_01.search.Tiefensuche;
 
 public class JUnitTest {
@@ -27,6 +27,35 @@ public class JUnitTest {
 	Node<Integer> o = new Node<Integer>("O", 8);
 	Node<Integer> p = new Node<Integer>("P", 5);
 
+	// Breitensuche: [A, B, C, D, E, F, G, L, M, O, P, H, I, J, K, N]
+
+	@Test
+	public void simulationBreitensuche() {
+		a.addChild(b);
+		a.addChild(c);
+		a.addChild(d);
+		b.addChild(e);
+		b.addChild(f);
+		b.addChild(g);
+		e.addChild(h);
+		e.addChild(i);
+		f.addChild(j);
+		f.addChild(k);
+		c.addChild(l);
+		c.addChild(m);
+		m.addChild(n);
+		d.addChild(o);
+		d.addChild(p);
+
+		Graph<Integer> graph = new Graph<Integer>(a);
+		SearchStrategy<Integer> breit = new Breitensuche<Integer>();
+		graph.search(breit, 11);
+		NodeList<Integer> list = breit.getPath();
+
+		assertTrue(list.toString().equals(
+				"[A, B, C, D, E, F, G, L, M, O, P, H, I, J, K, N]"));
+	}
+
 	@Test
 	public void testBreitensuche() {
 		a.addChild(b);
@@ -44,32 +73,18 @@ public class JUnitTest {
 		m.addChild(n);
 		d.addChild(o);
 		d.addChild(p);
+
 		Graph<Integer> graph = new Graph<Integer>(a);
-		NodeList<Integer> list = new NodeListImpl<Integer>();
-		Breitensuche breit = new Breitensuche();
-		NodeList<Integer> liste2 = graph.search(breit, 11, a);
-		NodeList<Integer> liste3 = breit.getPath();
-		list.add(a);
-		list.add(b);
-		list.add(c);
-		list.add(d);
-		list.add(e);
-		list.add(f);
-		list.add(g);
-		list.add(l);
-		list.add(m);
-		list.add(o);
-		list.add(p);
-		list.add(h);
-		list.add(i);
-		list.add(j);
-		list.add(k);
-		list.add(n);
-		assertEquals(list.equals(liste3), true);
+		SearchStrategy<Integer> breit = new Breitensuche<Integer>();
+		NodeList<Integer> list = graph.search(breit, 3);
+
+		assertTrue(list.toString().equals("[C, E]"));
 	}
 
+	// Tiefensuche: [A, B, E, H, I, F, J, K, G, C, L, M, N, D, O, P]
+
 	@Test
-	public void testBreitensuche2() {
+	public void simulationTiefensuche() {
 		a.addChild(b);
 		a.addChild(c);
 		a.addChild(d);
@@ -85,17 +100,18 @@ public class JUnitTest {
 		m.addChild(n);
 		d.addChild(o);
 		d.addChild(p);
+
 		Graph<Integer> graph = new Graph<Integer>(a);
-		NodeList<Integer> list = new NodeListImpl<Integer>();
-		Breitensuche breit = new Breitensuche();
-		NodeList<Integer> liste2 = graph.search(breit, 3, a);
-		list.add(c);
-		list.add(e);
-		assertEquals(list.equals(liste2), true);
+		SearchStrategy<Integer> tief = new Tiefensuche<Integer>();
+		graph.search(tief, 11);
+		NodeList<Integer> list = tief.getPath();
+
+		assertTrue(list.toString().equals(
+				"[A, B, E, H, I, F, J, K, G, C, L, M, N, D, O, P]"));
 	}
 
 	@Test
-	public void testTiefenSuche() {
+	public void testTiefensuche() {
 		a.addChild(b);
 		a.addChild(c);
 		a.addChild(d);
@@ -111,54 +127,12 @@ public class JUnitTest {
 		m.addChild(n);
 		d.addChild(o);
 		d.addChild(p);
+		
 		Graph<Integer> graph = new Graph<Integer>(a);
-		NodeList<Integer> list = new NodeListImpl<Integer>();
-		Tiefensuche tief = new Tiefensuche();
-		NodeList<Integer> liste2 = graph.search(tief, 11, a);
-		NodeList<Integer> liste3 = tief.getPath();
-		list.add(a);
-		list.add(b);
-		list.add(e);
-		list.add(h);
-		list.add(i);
-		list.add(f);
-		list.add(j);
-		list.add(k);
-		list.add(g);
-		list.add(c);
-		list.add(l);
-		list.add(m);
-		list.add(n);
-		list.add(d);
-		list.add(o);
-		list.add(p);
-		assertEquals(list.equals(liste3), true);
-	}
+		SearchStrategy<Integer> tief = new Tiefensuche<Integer>();
+		NodeList<Integer> list = graph.search(tief, 3);
 
-	@Test
-	public void testTiefenSuche2() {
-		a.addChild(b);
-		a.addChild(c);
-		a.addChild(d);
-		b.addChild(e);
-		b.addChild(f);
-		b.addChild(g);
-		e.addChild(h);
-		e.addChild(i);
-		f.addChild(j);
-		f.addChild(k);
-		c.addChild(l);
-		c.addChild(m);
-		m.addChild(n);
-		d.addChild(o);
-		d.addChild(p);
-		Graph<Integer> graph = new Graph<Integer>(a);
-		NodeList<Integer> list = new NodeListImpl<Integer>();
-		Tiefensuche tief = new Tiefensuche();
-		NodeList<Integer> liste2 = graph.search(tief, 3, a);
-		list.add(e);
-		list.add(c);
-		assertEquals(list.equals(liste2), true);
+		assertTrue(list.toString().equals("[E, C]"));
 	}
 
 }
